0e6d97d7e8cdfbbcc7cef5a4a3d88e43a92d20bb
Julio de la Matta <juliomattacadenas@gmail.com>
2020-02-21 16:30:35 +0100

    minor changes

---
+++ src/Nav.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Link } from "svelte-routing";
+</script>
+
+<nav class="navbar-light bg-dark px-5 py-0">
+  <div class="row">
+    <div class="col">
+      <Link to="/">
+        <div class="py-0">
+          <img
+            class="float-left logo"
+            src="img/logo-transparente.png"
+            width="170"
+            alt="KBS Music" />
+        </div>
+      </Link>
+    </div>
+  </div>
+</nav>
